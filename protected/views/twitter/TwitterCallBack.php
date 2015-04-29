1a1527633c535a1cf2b61c2a639e120a98a97469
oscar mesa <oscarmesa.elpoli@gmail.com>
2015-04-29 02:07:16 -0500

    subida 1

---
+++ protected/views/twitter/TwitterCallBack.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this TwitterController */
+
+$this->breadcrumbs=array(
+	'Twitter'=>array('/twitter'),
+	'TwitterCallBack',
+);
+?>
+<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+
+<p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>
